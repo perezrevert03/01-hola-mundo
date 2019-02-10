4289f9c5bb265d52b4c1b4937a896d286baf623b
Carles Pérez Revert <carlesperezmxt37@gmail.com>
2019-02-10 03:08:12 +0100

    Proyecto hola mundo terminado

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
 })
 export class BodyComponent {
 
+    mostrar = true;
+
+    frase: any = {
+        autor: 'Mariano Rajoy',
+        mensaje: 'Es el vecino el que elige al alcalde y es el alcalde el que quiere que sean los vecinos el alcalde'
+    };
+
+    fachas: string[] = ['Abascal', 'Casado', 'Rivera'];
 }
